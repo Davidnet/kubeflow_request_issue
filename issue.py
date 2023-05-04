3d7e99538ddf2120d9b7325eed8d84cc172e5251
davidnet <david.cardozo@me.com>
2023-05-04 13:15:59 -0400

    [Init] Initialization of project

---
+++ issue.py
@@ -0,0 +1,19 @@
+# %%
+from kfp import dsl
+
+
+@dsl.component
+def print_component(str_exmpl: str) -> None:
+    print(str_exmpl)
+
+
+@dsl.pipeline
+def pipeline():
+    print_task = print_component(str_exmpl="Hello World!")
+
+
+if __name__ == "__main__":
+    import kfp
+
+    print(kfp.__version__)
+    kfp.compiler.Compiler().compile(pipeline, "pipeline.yaml")
